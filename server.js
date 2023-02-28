ecf1d0ba8316d404afce61ada07958c870315bf0
Mustafa Gh <mustafa@abacus.co>
2023-02-28 11:22:49 +1100

    update

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict";
+const express = require("express");
+// Constants
+const PORT = 8080;
+const HOST = "127.0.0.1";
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("I'm node server\n");
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
